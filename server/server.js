e9dc37028c9230482b4f89678ff04c5166cf0030
Orca-Itgit config --global user.email whitefox@kakao.com <whitefox@kakao.com>
2022-09-29 10:01:05 +0900

    로그인 요청 통신 구현

--- server/server.js
+++ server/server.js
@@ -1,6 +1,25 @@
+/** Server 입니다. */
+
+const http = require('http');
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
-app.listen(9090, () => {
-    console.log('listening on 8080');
+app.use(express.urlencoded({extended: false}));
+app.use(express.json()); 
+app.use(cors(
+    {
+        'Access-Control-Allow-Origin' : '*'
+    }
+));
+
+
+app.listen(9200, () => {
+    console.log('listening on 9200');
+});
+
+app.post('/login', (req, res) => {
+    console.log(req.body);
+    res.send('로그인 성공');
 });
